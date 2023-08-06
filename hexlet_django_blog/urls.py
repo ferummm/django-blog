6de4634d87a03cbb3ba993ddf9422ce37beff9ca
Andreeva Polina <ferummm.it@gmail.com>
2023-08-06 14:40:38 +0300

    Extended the base template for the article application

--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from hexlet_django_blog import views
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.index),
     path('about/', views.about),
+    path('articles/', include('hexlet_django_blog.article.urls')),
     path('admin/', admin.site.urls),
 ]

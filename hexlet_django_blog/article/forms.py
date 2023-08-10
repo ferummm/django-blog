0f29c2f0f6ab90553aa83474c77ab3c932c8e6b3
Andreeva Polina <ferummm.it@gmail.com>
2023-08-10 14:31:50 +0300

    fix import error

--- hexlet_django_blog/article/forms.py
+++ hexlet_django_blog/article/forms.py
@@ -1,13 +1,13 @@
 from django import forms 
 from django.forms import ModelForm
-from .models import ArticleComment, Article
+from .models import Article, Comment
 
 class CommentArticleForm(forms.Form):
     content = forms.CharField(label='Комментарий')
 
 class ArticleCommentForm(ModelForm):
     class Meta:
-        model = ArticleComment
+        model = Comment
         fields = ['content']
 
 class ArticleForm(ModelForm):
